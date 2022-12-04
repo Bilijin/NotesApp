68d782ce0b8bc15e3b7ed00374fa80a56d2763a1
Bolaji Alabi <alabibolaji78@gmail.com>
2022-12-04 22:07:05 +0100

    added navigation to app

--- app/src/main/java/com/mobolajia/notesapp/ui/MainActivity.kt
+++ app/src/main/java/com/mobolajia/notesapp/ui/MainActivity.kt
@@ -2,11 +2,18 @@ package com.mobolajia.notesapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
 import com.mobolajia.notesapp.R
+import com.mobolajia.notesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
